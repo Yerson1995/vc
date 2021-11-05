07ce5f67de2080d768470445c42fc0bae26eab61
Oscar Andres Mancera Garzón <30636302+Alasienn@users.noreply.github.com>
2021-11-04 19:11:41 -0500

    3

--- content/sketches/troxler.js
+++ content/sketches/troxler.js
@@ -1,4 +1,6 @@
 let img;
+let facemesh;
+let predictions = [];
 
 function setup() {
   
@@ -11,3 +13,42 @@ function setup() {
   img.hide(); 
   frameRate(1); 
 }
+
+// when the image is ready, then load up poseNet
+function imageReady() {
+    facemesh = ml5.facemesh(modelReady);
+  
+    facemesh.on("predict", results => {
+      predictions = results;
+    });
+  }
+  
+  // when poseNet is ready, do the detection
+  function modelReady() {
+    console.log("Model ready!");
+    facemesh.predict(img);
+  }
+  
+  // draw() will not show anything until poses are found
+  function draw() {
+    if (predictions.length > 0) {
+      image(img, 0, 0, width, height);
+      drawKeypoints();
+      noLoop(); // stop looping when the poses are estimated
+    }
+  }
+  
+  // A function to draw ellipses over the detected keypoints
+  function drawKeypoints() {
+    for (let i = 0; i < predictions.length; i += 1) {
+      const keypoints = predictions[i].scaledMesh;
+  
+      // Draw facial keypoints.
+      for (let j = 0; j < keypoints.length; j += 1) {
+        const [x, y] = keypoints[j];
+  
+        fill(0, 255, 0);
+        ellipse(x, y, 5, 5);
+      }
+    }
+  }
