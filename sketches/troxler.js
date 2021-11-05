c399dce13c5815b451681682c749f8711f341daa
Alasienn <Alasienn@users.noreply.github.com>
2021-11-05 00:23:36 +0000

    deploy: aeffa022a6c481b517a157789b4eda2306adbc48

--- sketches/troxler.js
+++ sketches/troxler.js
@@ -1,54 +1,16 @@
 let img;
-let facemesh;
-let predictions = [];
+
 
 function setup() {
   
   createCanvas(494, 361);
-
   
-  img = createImg("/docvc/docs/sketches/troxler_main.png", imageReady);
-  
-
-  img.hide(); 
-  frameRate(1); 
+  img = loadImage('content\sketches\troxler_main.png'); // Load the image
 }
 
-// when the image is ready, then load up poseNet
-function imageReady() {
-    facemesh = ml5.facemesh(modelReady);
-  
-    facemesh.on("predict", results => {
-      predictions = results;
-    });
-  }
-  
-  // when poseNet is ready, do the detection
-  function modelReady() {
-    console.log("Model ready!");
-    facemesh.predict(img);
-  }
-  
-  // draw() will not show anything until poses are found
-  function draw() {
-    if (predictions.length > 0) {
-      image(img, 0, 0, width, height);
-      drawKeypoints();
-      noLoop(); // stop looping when the poses are estimated
-    }
-  }
-  
-  // A function to draw ellipses over the detected keypoints
-  function drawKeypoints() {
-    for (let i = 0; i < predictions.length; i += 1) {
-      const keypoints = predictions[i].scaledMesh;
-  
-      // Draw facial keypoints.
-      for (let j = 0; j < keypoints.length; j += 1) {
-        const [x, y] = keypoints[j];
-  
-        fill(0, 255, 0);
-        ellipse(x, y, 5, 5);
-      }
-    }
-  }
+function draw() {
+  // Displays the image at its actual size at point (0,0)
+  image(img, 0, 0);
+  // Displays the image at point (0, height/2) at half size
+  image(img, 0, height / 2, img.width / 2, img.height / 2);
+}
